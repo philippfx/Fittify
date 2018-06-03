a219a526a1db17203c5aa6d11ef9121f8d110e0d
PhilippPrivate <PhilippPrivate@DESKTOP-87V5QMQ>
2018-06-03 15:50:12 +0200

    Start testing fittify.client.apimodelrepository

---
+++ Fittify.Client.ApiModelRepositories.Test/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Formatting = System.Xml.Formatting;
+
+namespace Fittify.Client.ApiModelRepositories.Test
+{
+    [TestFixture]
+    class CategoryOfmRepositoryShould
+    {
+        private readonly Guid _ownerGuid = new Guid("00000000-0000-0000-0000-000000000000");
+        
+        [Test]
+        public async Task ReturnSingleOfmGetById()
+        {
+            // Arrange
+            
+            // Act
+
+            // Assert
+            
+        }
+    }
+}
